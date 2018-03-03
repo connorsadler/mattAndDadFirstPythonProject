2ded422b6dc508528a3c39f3c176c1f465475fa5
Connor <Connor@DESKTOP-IF0PH2C.mshome.net>
2018-03-03 18:26:31 +0000

    Added simple man moving around screen in gameclient.py

--- src/root/nested/multiplayergame/Chat room.py
+++ src/root/nested/multiplayergame/Chat room.py
@@ -62,48 +62,66 @@ def broadcast(msg, prefix=""):  # prefix is for name identification.
         except ConnectionResetError:
             print("ConnectionResetError - skipping this client")
 
+
 class MiThread(threading.Thread):   
     def __init__(self):
         threading.Thread.__init__(self)
 
     def run(self):
-        mylist = ['hello', 'body', 'zombie', 'toilet', 'flush', 'world', 'cheese' ]
         while True:
             global clients
             print("Background thread checking server status")
             print("  client count: " + str(len(clients)))
-            broadcast(bytes("Server is still running - hi there", "utf-8"))
-            wordIndex = random.randint(0, len(mylist)-1)
-            randomWord = mylist[wordIndex]
-            broadcast(bytes("Please type the word: " + randomWord, "utf-8"))
+            broadcast("Server is still running - hi there")
+            serverTick()
             time.sleep(5)
-            self.broadcastScores()
     
-    def broadcastScores(self):
-        scoresString = ""
-        for name in scores.keys():
-            scoresString += ", " + name + ": " + str(scores[name])
-        broadcast(bytes("Scores: " + scoresString, "utf-8"))
-
-
-
 #
 # Game logic
 #
+
 # dict of "name" (player name, string) -> score (integer)
 scores = {}
 
-# Any message from any client comes in here
-def handleMessageFromClient(name):
-    global scores
-    print("handleMessageFromClient, name: " + name + "\n")
+def addOneToScoreForPlayer(name):
     if name in scores:
         scoreForThisPlayer = scores[name]
     else:
         scoreForThisPlayer = 0
     scoreForThisPlayer += 1
     scores[name] = scoreForThisPlayer
+
+# Any message from any client comes in here
+def handleMessageFromClient(name):
+    print("handleMessageFromClient, name: " + name + "\n")
+    print("randomWord: " + randomWord + "\n")
+    addOneToScoreForPlayer(name)
     broadcast(name + ", you scored a point, well done")
+
+# the current word the players need to guess
+randomWord = "none"
+mylist = ['hello', 'body', 'zombie', 'toilet', 'flush', 'world', 'cheese' ]
+
+# Called by server code every 5 seconds
+def serverTick():
+    # allow write access to our global variable to store the current word the players need to guess
+    global randomWord 
+
+    # tell players the current scores
+    broadcastScores()
+
+    # choose a new random word from the list
+    wordIndex = random.randint(0, len(mylist)-1)
+    randomWord = mylist[wordIndex]
+    # tell players the new word
+    broadcast("Please type the word: " + randomWord)
+    
+def broadcastScores():
+    scoresString = ""
+    for name in scores.keys():
+        scoresString += ", " + name + ": " + str(scores[name])
+    broadcast(bytes("Scores: " + scoresString, "utf-8"))
+
         
 #    
 # If you run this file directly, this is the code that will execute
--- src/root/nested/multiplayergame/gameclient.py
+++ src/root/nested/multiplayergame/gameclient.py
@@ -3,4 +3,98 @@ from tkinter import *
 
 TIMESTEP = 10
 
-print("hello world\n")
+print("gameclient starts")
+
+
+
+
+
+ #----------------------------
+# Minecraft 2D
+#----------------------------
+
+import pygame, sys, random
+from pygame.locals import *
+from variables import *
+
+fpsClock = pygame.time.Clock()
+
+#variables for the map size
+TILESIZE  = 3
+MAPWIDTH  = 100
+MAPHEIGHT = 30
+
+#the position of the player [x,y]
+playerPos = [0,0]
+
+#use list comprehension to create our tilemap
+#tilemap = [ [DIRT for w in range(MAPWIDTH)] for h in range(MAPHEIGHT) ] 
+
+#set up the display
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((MAPWIDTH * TILESIZE, MAPHEIGHT*TILESIZE))
+
+#loop through each row
+# for rw in range(MAPHEIGHT):
+#     #loop through each column in that row
+#     for cl in range(MAPWIDTH):
+#         #pick a random number between 0 and 10
+#         randomNumber = random.randint(0,10)
+#         #WATER if the random number is a 1 or a 2
+#         if randomNumber in [1,2]:
+#             tile = WATER
+#         #GRASS if the random number is a 3 or a 4
+#         elif randomNumber in [3,4]:
+#             tile = GRASS
+#         #otherwise it's DIRT
+#         else:
+#             tile = DIRT
+#         #set the position in the tilemap to the randomly chosen tile
+#         tilemap[rw][cl] = tile
+
+pygame.key.set_repeat(5)
+                      
+while True:
+
+    #fill the background in black        
+    DISPLAYSURF.fill(BLACK)
+
+    #get all the user events
+    for event in pygame.event.get():
+        #if the user wants to quit
+        if event.type == QUIT:
+            #and the game and close the window
+            pygame.quit()
+            sys.exit()
+        #if a key is pressed
+        elif event.type == KEYDOWN:
+            #if the right arrow is pressed
+            if event.key == K_RIGHT and playerPos[0] < MAPWIDTH - 1:
+                #change the player's x position
+                playerPos[0] += 1
+            if event.key == K_LEFT and playerPos[0] > 0:
+                #change the player's x position
+                playerPos[0] -= 1
+            if event.key == K_UP and playerPos[1] > 0:
+                #change the player's x position
+                playerPos[1] -= 1
+            if event.key == K_DOWN and playerPos[1] < MAPHEIGHT -1:
+                #change the player's x position
+                playerPos[1] += 1
+                    
+#     #loop through each row
+#     for row in range(MAPHEIGHT):
+#         #loop through each column in the row
+#         for column in range(MAPWIDTH):
+#             #draw the resource at that position in the tilemap, using the correct image
+#             DISPLAYSURF.blit(textures[tilemap[row][column]], (column*TILESIZE,row*TILESIZE))
+        
+    #display the player at the correct position 
+    DISPLAYSURF.blit(PLAYER,(playerPos[0] * TILESIZE,
+                             playerPos[1] * TILESIZE)
+                     )
+
+    #update the display
+    pygame.display.update()
+    #create a short delay
+    fpsClock.tick(24)
