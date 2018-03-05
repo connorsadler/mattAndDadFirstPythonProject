56ea76177424c0e0b52c3eb2556ff3b2d9867ec1
Connor <Connor@DESKTOP-IF0PH2C.mshome.net>
2018-03-05 21:32:55 +0000

    More work

--- src/root/nested/matty/pygamematty.py
+++ src/root/nested/matty/pygamematty.py
@@ -13,29 +13,47 @@ red = (150,10,15)
 
 clock=pygame.time.Clock()
 
-crashed=False
+
 carImg = pygame.image.load('racecar.png')
 
 def car(x,y):
     gameDisplay.blit(carImg, (x,y))
 
-x =  (display_width * 0.45)
-y = (display_height * 0.8)
-
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-
-    gameDisplay.fill(red)
-    car(x,y)
+def game_loop():
+    x =  (display_width * 0.45)
+    y = (display_height * 0.8)
+    x_change = 0
+    car_speed = 0
+    car_width = 73
+    gameExit=False
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
     
-    if y > 0:
-        y = y - 10
-        x = x - 5
-    
-    pygame.display.update()
-    clock.tick(60)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+                    
+        x = x + x_change
+        
+        gameDisplay.fill(red)
+        car(x,y)
+        if x > display_width - car_width or x < 0:
+            gameExit = True
+        
+    #     if y > 0:
+    #         y = y - 10
+    #         x = x - 5
+        
+        pygame.display.update()
+        clock.tick(60)
 
+game_loop()
 pygame.quit()
 quit()
