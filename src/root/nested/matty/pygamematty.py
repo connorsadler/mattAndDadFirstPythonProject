c83b9823ca39ae8ed9e165a55dc3b58a9bef11f5
Connor <Connor@DESKTOP-IF0PH2C.mshome.net>
2018-03-06 20:54:34 +0000

    More work on showing text

--- src/root/nested/matty/pygamematty.py
+++ src/root/nested/matty/pygamematty.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 
@@ -19,6 +20,32 @@ carImg = pygame.image.load('racecar.png')
 def car(x,y):
     gameDisplay.blit(carImg, (x,y))
 
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text, fontsize):
+    largeText = pygame.font.Font('freesansbold.ttf',fontsize)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    
+    
+
+def crash(x,y):
+    message_display('You Crashed', 115)
+    time.sleep(2)
+    gameDisplay.fill(red)
+    car(x,y)
+    message_display('Prepare to Quit!!!', 80)
+    time.sleep(2)
+#     game_loop()
+    pygame.quit()
+    quit()
+    
 def game_loop():
     x =  (display_width * 0.45)
     y = (display_height * 0.8)
@@ -29,7 +56,8 @@ def game_loop():
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                gameExit = True
+                pygame.quit()
+                quit()
     
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
@@ -45,7 +73,7 @@ def game_loop():
         gameDisplay.fill(red)
         car(x,y)
         if x > display_width - car_width or x < 0:
-            gameExit = True
+            crash(x,y)
         
     #     if y > 0:
     #         y = y - 10
