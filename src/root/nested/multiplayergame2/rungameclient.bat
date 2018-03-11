22643519d6fc0efb1cad2a3bff210cae95d428c4
Rhi <Rhi@Rhi-Windows7-PC>
2018-03-11 21:51:56 +0000

    rungameclient.bat

---
+++ src/root/nested/multiplayergame2/rungameclient.bat
@@ -0,0 +1,9 @@
+
+pushd %~dp0
+
+PATH=C:\Users\Rhi\AppData\Local\Programs\Python\Python36-32;%PATH%
+PATH=C:\Users\Rhi\AppData\Local\Programs\Python\Python36-32\Scripts;%PATH%
+
+python "gameclient different machine.py"
+
+pause
